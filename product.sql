d1b91bea20fc42db59f6961d373d06d36678c563
rfschroeder <renanfernandos@gmail.com>
2023-05-10 01:22:45 -0300

    Update product.sql - CAPITALIZE

--- product.sql
+++ product.sql
@@ -1,5 +1,20 @@
+CREATE TEMP FUNCTION CAPITALIZE(str STRING) AS (( 
+  SELECT 
+    IF(str = '', '',
+      STRING_AGG(
+        CONCAT(
+          UPPER(SUBSTR(single_words,1,1)), 
+          LOWER(SUBSTR(single_words,2))
+        ), 
+        '' ORDER BY position
+      )
+    )
+  FROM UNNEST(REGEXP_EXTRACT_ALL(str, r' +|-+|.[^ -]*')) AS single_words
+  WITH OFFSET AS position
+));
+
 SELECT
-    stg.name AS mdmname,
+    CAPITALIZE(stg.name) AS mdmname,
     CONCAT("qarenan-", stg.name) AS mdmdescription,
     stg.price as mdmunitvalue,
     CASE
